e70ee50daf5056be8e8e47db9630ecf1095f023f
Elena Hadjimina <hadjimina.elena@gmail.com>
2020-12-02 21:09:09 +0800

    working on animations

--- js/monkeys.js
+++ js/monkeys.js
@@ -1,11 +1,18 @@
+
+const monkeyImage = new Image();
+monkeyImage.src = '/images/monkey/monkey-sprite.png';
+
 class Monkey {
   constructor(game, x, y, width, height) {
     this.game = game;
     this.x = x;
     this.y = y;
-    this.width = 20;
-    this.height = 20;
+    this.width = 30;
+    this.height = 30;
     this.color = "brown";
+
+    this.positionChangeTimestamp = 0;
+    this.position = 0;
   }
 
   runLogic() {
@@ -16,4 +23,28 @@ class Monkey {
     this.game.context.fillStyle = this.color;
     this.game.context.fillRect(this.x, this.y, this.width, this.height);
   }
+
+  /*draw() {
+    if (Date.now() > this.positionChangeTimestamp + 150) {
+      this.position = (this.position + 1) % 15;
+      this.positionChangeTimestamp = Date.now();
+    }
+    this.game.context.save();
+    this.game.context.translate(this.x + this.width, this.y);
+    this.game.context.scale(-1, 1);
+    this.game.context.drawImage(
+      monkeyImage,
+      140 * this.position,
+      0,
+      140,
+      140,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    this.game.context.restore();
+  }*/
+
+
 }
